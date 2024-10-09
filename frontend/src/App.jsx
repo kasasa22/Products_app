861037c171189e3e5c07a995d70d278b916561eb
kasasa22 <kasasatrevor25@gmail.com>
2024-10-10 01:41:03 +0300

    feat: frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,20 @@
-import React from 'react'
-import { Box} from '@chakra-ui/react'
-import {Route, Routes} from 'react-router-dom'
-import Homepage from './pages/Homepage'
-import Createpage from './pages/Createpage'
-import NavBar from './components/NavBar'
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
 
-const App = () => {
-  return (
-    <>
-     <Box minH={"100vh"}>
-       <NavBar/>
-       <Routes>
-        <Route path='/' element={<Homepage/>}/>
-        <Route path='/create' element={<Createpage/>}/>
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
-       </Routes>
-
-     </Box>
-    </>
-  )
+function App() {
+	return (
+		<Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/create' element={<CreatePage />} />
+			</Routes>
+		</Box>
+	);
 }
 
-export default App 
- 
+export default App;
